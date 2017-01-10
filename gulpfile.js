2c075b6ee0dff040f75717dfe0cbc91dae2bcecf
Pawel Korus <pawelkorus@ymail.com>
2017-01-10 17:15:07 +0100

    dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,39 @@
-var gulp = require('gulp')
-	,path = require('path')
-	,express = require('express')
-	,php = require('php-proxy-middleware')
-	,paths = {
-    wordpress: path.join(__dirname, 'wordpress')
+var gulp = require('gulp');
+var path = require('path');
+var express = require('express');
+var php = require('php-proxy-middleware');
+var wordpressInstaller = require('wordpress-installer');
+
+var paths = {
+  wordpress: path.join(__dirname, 'wordpress')
+};
+
+var options = {
+	php: {
+		root: paths.wordpress
+		,port: 11000
+		,address: '127.0.0.1'
+		,execPath: 'php' // path to php
+	},
+	wordpress: {
+		rootPath: paths.wordpress
+		,dbName: 'wordpress'
+		,dbUser: 'user'
+		,dbPassword: 'password'
+		,tablePrefix: 'wp_'
 	}
-	,options = {
-		php: {
-			root: paths.wordpress
-			,port: 11000
-			,address: '127.0.0.1'
-		}
-	}
-	,files = {
-	}
-	;
+};
+
+var files = {
+};
+
+gulp.task('ensure-wordpress', function() {
+	wordpressInstaller(options.wordpress).ensure();
+});
 
 gulp.task('build', []);
 
-gulp.task('startPhp', [], function(cb) {
+gulp.task('startPhp', ['ensure-wordpress'], function(cb) {
 	var app = express();
 
 	app.use(php(options.php));
