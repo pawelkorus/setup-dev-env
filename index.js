9083a0bf6e85e3da4f1fba3160d59311208d6db5
Pawel Korus <pawelkorus@ymail.com>
2017-02-09 00:07:06 +0100

    env module

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = exports = {
 	vagrant: require('./vagrant')
 	,proxy: require('./proxy')
+	,env: require('./env')
 }
--- vagrant/index.js
+++ vagrant/index.js
@@ -4,8 +4,7 @@ var path = require('path');
 var promise = require('promise');
 
 module.exports = exports = {
-	init: function(options) { return wrap.bind(null, options, init); }
-	,up: function(options) { return wrap.bind(null, options, up); }
+	up: function(options) { return wrap.bind(null, options, up); }
 	,suspend: function(options) { return wrap.bind(null, options, suspend); }
 	,destroy: function(options) { return wrap.bind(null, options, destroy); }
 }
@@ -14,16 +13,6 @@ function wrap(options, func) {
 	return new Promise(function(resolve, reject) {
 		func(options, resolve, reject);
 	});
-}
-
-function init(options, resolve, reject) {
-	options = options || {};
-	if(!options.env) throw new Error("Vagrant environment not selected.");
-	var reader = fs.createReadStream(path.join(__dirname, options.env, 'Vagrantfile'));
-	var writer = fs.createWriteStream('Vagrantfile');
-	reader.pipe(writer);
-	writer.on('finish', resolve);
-	writer.on('error', reject);
 }
 
 function up(options, resolve, reject) {
